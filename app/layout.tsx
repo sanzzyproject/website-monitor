a372886d21d9c031322897e6f2c849821ce4c074
sanzzyproject <stokakunsanzz250@gmail.com>
2026-01-14 16:28:55 +0700

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import { Providers } from './providers'
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { Toaster } from "@/components/ui/toaster"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Ping-Notify',
+  description: 'Monitor your websites and get notified when they go down',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png'
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers><Toaster />
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import HomePage from '@/components/homepage'
+
+export const metadata = {
+  title: 'PingNotify - Website Uptime Monitoring',
+  description: 'Monitor your websites\' uptime and get instant notifications with PingNotify.',
+}
+
+export default function Page() {
+  return <HomePage />
+}
